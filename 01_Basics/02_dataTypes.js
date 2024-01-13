5738ac6c70a4a50fa0b4f8e77d3c44890eb3c568
Kartik Sarwan <kartiksarwan2017.comp@mmcoe.edu.in>
2024-01-13 18:49:33 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,55 @@
+"use strict"; // treat all JS code as newer version
+
+/* we can use alert() either by binding JS with html or in developer tools
+ everything that we write in the console is in the document
+ JS Engine is present inside the browser
+
+ there is a different syntax to use alert in the node
+*/
+// alert("Hare Krishna"); // we are using nodejs, not browser
+
+/* Most of the files in react or node we are going to avoid semicolons
+in coding the most important thing is readability
+
+JS automatically avoid spaces enter in between the methods
+
+*/
+
+/* Code Readability should be high
+don't use this way */
+// console.log(3
+//     +4) 
+
+// console.log("Krishna")
+
+
+/* Documentation Use 
+https://tc39.es/ecma262/#sec-intro Original JS Documentation
+
+https://developer.mozilla.org/en-US/docs/Learn/JavaScript
+MDN DOCS
+
+*/
+
+let name = "Krishna";
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+/* Data Types in JS */
+
+// number =>  2 to power 53
+// bigint => 
+// string => ""
+// boolean => true / false
+// null => standalone value => representation of empty value
+// undefined => define variable but not assigned value
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Krishna");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
